113f45b6a3c2bdaddee104f7feeeed0c12de5347
Carson Yan <carson.yan@crypto.com>
2019-04-30 17:21:26 +0800

    🔥 Removed routing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AngularFontAwesomeModule } from "angular-font-awesome";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { TxnHistoryComponent } from "./components/txn-history/txn-history.component";
 
